ffae14c22caad0bf8726e9e586122950b40529bd
temple7447 <starukido@gmail.com>
2025-05-06 20:49:57 +0100

    feat: update BASE_URL to point to the new backend service

--- src/services/api/baseApi.ts
+++ src/services/api/baseApi.ts
@@ -2,7 +2,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { RootState } from '../store/store';
 
-export const BASE_URL = "https://market-vendor.onrender.com";
+export const BASE_URL = "https://campus-trade-bakend.onrender.com";
+// export const BASE_URL = "https://market-vendor.onrender.com";
 // export const BASE_URL = "http://localhost:3000";
 
 export type TagTypes = 'Categories' | 'Products' | 'Users' | 'Orders' | 'Profile' | 'Sellers';
