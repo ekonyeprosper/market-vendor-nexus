0c0cdda2910f954e75e2b1ea5e204960f9e340a8
temple7447 <starukido@gmail.com>
2025-05-28 00:59:36 +0100

    chore: Clean up commented-out BASE_URL options in baseApi.ts

--- src/services/api/baseApi.ts
+++ src/services/api/baseApi.ts
@@ -4,9 +4,9 @@ import { useEffect } from 'react';
 import { RootState } from '../store/store';
 import { logout } from '../store/slices/authSlice';
 
-// export const BASE_URL = "https://campus-trade-bakend.onrender.com";
+export const BASE_URL = "https://campus-trade-bakend.onrender.com";
 // export const BASE_URL = "https://market-vendor.onrender.com";
-export const BASE_URL = "http://16.170.80.202:3000";
+// export const BASE_URL = "https://16.170.80.202:3000";
 // export const BASE_URL = "http://localhost:3000";
 
 export type TagTypes = 'Categories' | 'Products' | 'Users' | 'Orders' | 'Profile' | 'Sellers';
