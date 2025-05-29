855674df01c73e3ed36b2b05d9e14b1b05ac214f
temple7447 <starukido@gmail.com>
2025-05-29 01:28:44 +0100

    chore: Update BASE_URL to point to the correct staging endpoint

--- src/services/api/baseApi.ts
+++ src/services/api/baseApi.ts
@@ -5,8 +5,8 @@ import { RootState } from '../store/store';
 import { logout } from '../store/slices/authSlice';
 
 // export const BASE_URL = "https://campus-trade-bakend.onrender.com";
-// export const BASE_URL = "https://market-vendor.onrender.com";
-export const BASE_URL = "https://customerbackend.online";
+export const BASE_URL = "https://market-vendor.onrender.com";
+// export const BASE_URL = "https://customerbackend.online";
 // export const BASE_URL = "http://localhost:3000";
 
 export type TagTypes = 'Categories' | 'Products' | 'Users' | 'Orders' | 'Profile' | 'Sellers';
