bf86840feceabe2513f717b1413016df12848283
temple7447 <starukido@gmail.com>
2025-05-30 20:12:17 +0100

    chore: Update store configuration to include all API reducers and middlewares

--- src/services/store/store.ts
+++ src/services/store/store.ts
@@ -1,14 +1,31 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { baseApi } from '../api/baseApi';
 import authReducer from './slices/authSlice';
+import { authApi } from '../api/authApi';
+import { productsApi } from '../api/productsApi';
+import { ordersApi } from '../api/ordersApi';
+import { categoriesApi } from '../api/categoriesApi';
+import { userApi } from '../api/userApi';
+import { adminApi } from '../api/adminApi';
 
 export const store = configureStore({
   reducer: {
-    [baseApi.reducerPath]: baseApi.reducer,
     auth: authReducer,
+    [authApi.reducerPath]: authApi.reducer,
+    [productsApi.reducerPath]: productsApi.reducer,
+    [ordersApi.reducerPath]: ordersApi.reducer,
+    [categoriesApi.reducerPath]: categoriesApi.reducer,
+    [userApi.reducerPath]: userApi.reducer,
+    [adminApi.reducerPath]: adminApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(baseApi.middleware),
+    getDefaultMiddleware().concat(
+      authApi.middleware,
+      productsApi.middleware,
+      ordersApi.middleware,
+      categoriesApi.middleware,
+      userApi.middleware,
+      adminApi.middleware
+    ),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
